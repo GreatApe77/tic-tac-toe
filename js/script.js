62c1ff8eb989b60c608df11a82516fea16b19504
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-08-12 02:21:14 -0300

    card de jogador definido

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+/*
+        <div class="jogador-1">
+            <h4>Jogador 1:</h4>
+            <p>Nome: <strong>Mateus</strong></p>
+
+            <p>pontos: <strong>2</strong></p>
+        </div>
+        <div class="jogador-2">
+            <h4>Jogador 2:</h4>
+            
+            <p>Nome: <strong>Eduardo</strong></p>
+            <p>pontos: <strong>2</strong></p>
+        </div>
+*/
